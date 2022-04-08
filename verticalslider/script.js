c18e72103c327e6ff017dd6caa3e2583a0cc8c7b
Cameron Reed <cameronreed.tech@gmail.com>
2022-04-08 16:53:02 -0600

    Need to get picture to match slide &  debug JS

--- verticalslider/script.js
+++ verticalslider/script.js
@@ -28,6 +28,4 @@ const changeSlide = (direction) => {
     
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
     slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-
-
-}
+};
